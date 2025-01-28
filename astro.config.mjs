430b4dd173492eb58b7dd4bc0221f6ee149527be
BeeBarry <barry.namdari@gmail.com>
2025-01-28 12:31:52 +0100

    Configured Astro for GitLab Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  
+  site: 'https://personalpro4.gitlab.io/website',
+  base: '/Website',
+  outDir: 'public',
+  publicDir: 'static'
+});
