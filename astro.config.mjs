86b972fc39d676362d9e681fbf61c4da58b61a3d
BeeBarry <barry.namdari@gmail.com>
2025-07-14 17:35:36 +0200

    Added React and Tailwind Css ( via vite ) to this project. Also verified functionality of said added thingss.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,17 @@ import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
 
+import react from '@astrojs/react';
+
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   site: 'https://barrynamdari.se',
   outDir: './public',
   publicDir: './static',
-  integrations: [mdx()]
+  integrations: [mdx(), react()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
