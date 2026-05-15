cfbc817b7713b00f8acf6cbbeba19ce90971a665
BeeBarry <barry.namdari@gmail.com>
2026-05-15 11:32:51 +0200

    pre refacoktoring, checkpoint-comitting previuis uncomited stuff.

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,4 +1,5 @@
 
 export default new Map([
+["src/content/learn/azure-basics.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Flearn%2Fazure-basics.mdx&astroContentModuleFlag=true")],
 ["src/content/learn/intro-to-docker.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Flearn%2Fintro-to-docker.mdx&astroContentModuleFlag=true")]]);
 		
